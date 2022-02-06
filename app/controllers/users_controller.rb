169c45e601c47c974d8045411be392188efbdbaa
atsushi oshima <aboutagial@yahoo.co.jp>
2022-02-06 23:17:13 +0900

    user登録・編集デザイン修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
       @monthly_view = params[:monthly_view]
     end
 
-    # @costs = @fixed_costs.joins(:categories).group("categories.cat_name").sum(:payment).sort_by { |_, v| v }.reverse.to_h
     # まず月額の支出だけまとめる（１）
     # all_monthlies = @fixed_costs.joins(:categories).where("monthly_annual = ?", 0).group("categories.cat_name").sum(:payment)
     all_monthlies = @fixed_costs.joins(:categories).where(monthly_annual: 0).group("categories.cat_name").sum(:payment)
--- app/helpers/fixed_costs_helper.rb
+++ app/helpers/fixed_costs_helper.rb
@@ -10,4 +10,17 @@ module FixedCostsHelper
   def annual_payment(pay)
     pay.payment*12
   end
+
+  def switch_annual_monthly(total_annual, total_monthly, monthly_view, fixed_cost)
+    if monthly_view == "true" && fixed_cost.monthly_annual == "annual"
+      total_monthly << monthly_payment(fixed_cost)
+    elsif monthly_view == "true" && fixed_cost.monthly_annual == "monthly"
+      total_monthly << fixed_cost.payment
+    elsif monthly_view == "false" && fixed_cost.monthly_annual == "monthly"
+      total_annual << annual_payment(fixed_cost)
+    else
+      total_annual << fixed_cost.payment
+    end
+    return total_annual, total_monthly
+  end
 end
