a908c2bcd9efb35f155af0fe3080e5c6acc08e9e
atsushi oshima <aboutagial@yahoo.co.jp>
2022-02-10 15:39:23 +0900

    comment入力欄デザイン変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 255, message: 'は255文字以内で登録してください' }
+  validates :content, presence: true, length: { maximum: 255, message: 'は255文字以内で登録してください' }
 end
