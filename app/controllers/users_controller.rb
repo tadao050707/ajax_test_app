90fe23e2368f3281f85427ce23b2cdda0e50c65c
atsushi oshima <aboutagial@yahoo.co.jp>
2022-03-04 10:16:01 +0900

    comment空送信時メッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,32 +12,35 @@ class UsersController < ApplicationController
     @comments = @user.comments
     @comment = @user.comments.build
     # flash.now[:notice1] = 'コメント内容を変更しました'
-    # params[:monthly_view] ? @monthly_view = "true" : params[:monthly_view]
 
+    # params[:monthly_view] ? @monthly_view = "true" : params[:monthly_view]
     if params[:monthly_view].nil?
       @monthly_view = "true"
     else
       @monthly_view = params[:monthly_view]
     end
 
-    # まず月額の支出だけまとめる（１）
+    # （１）まず月額の支出だけまとめる groupは同じカテゴリをまとめる sumはまとめたカテゴリ(payment)を合計する。
     all_monthlies = @fixed_costs.joins(:categories).where(monthly_annual: 0).group("categories.cat_name").sum(:payment)
-    # 年額の支出だけまとめる（２）
+    # （２）年額の支出だけまとめる
     all_annuals = @fixed_costs.joins(:categories).where(monthly_annual: 1).group("categories.cat_name").sum(:payment)
 
     # 月額表示の場合
     # （２）を月額に変換
     if @monthly_view == "true"
-      # binding.pry
-      all_annuals = all_annuals.map{|key, value| [key, value/12]}.to_h
+      changed_monthlies = all_annuals.map{|key, value| [key, value/12]}.to_h
+      # 上のall_annualsをchanged_monthliesにすると分かりやすい
       # （１）と（２）を合計し、月額として@costsに代入
-      @costs = all_monthlies.merge(all_annuals){|key, v1, v2| v1 + v2}.sort_by { |_, v| v }.reverse
+      @costs = all_monthlies.merge(changed_monthlies){|key, v1, v2| v1 + v2}.sort_by { |_, v| v }.reverse
+      #keyはカテゴリ名
     else
     # 年額表示の場合
       # （１）を年額に変換
-      all_monthlies = all_monthlies.map{|key, value| [key, value*12]}.to_h
+      changed_annuals = all_monthlies.map{|key, value| [key, value*12]}.to_h
       # （１）と（２）を合計し、月額として@costsに代入
-      @costs = all_monthlies.merge(all_annuals){|key, v1, v2| v1 + v2}.sort_by { |_, v| v }.reverse
+      # @costs = all_monthlies.merge(changed_annuals){|key, v1, v2| v1 + v2}.sort_by { |_, v| v }.reverse
+      @costs = all_annuals.merge(changed_annuals){|key, v1, v2| v1 + v2}.sort_by { |_, v| v }.reverse
+
     end
   end
 
