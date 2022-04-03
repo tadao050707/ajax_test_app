3b010f6f777bc485dd6b3ccb6efed816742f262c
atsushi oshima <aboutagial@yahoo.co.jp>
2022-04-03 18:14:17 +0900

    通知機能リファクタ

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  include NotificationsHelper
+
   def index
     # current_userの投稿に紐づいた通知一覧
     @notifications = current_user.passive_notifications
@@ -7,10 +9,7 @@ class NotificationsController < ApplicationController
     end
     # 自分がコメントした人の固定支出のIDを取ってくる
     comment_ids = Notification.where(visitor_id: current_user.id).pluck(:user_id)
-    @notifications = @notifications.where(user_id: current_user.id).or(@notifications.where(user_id: comment_ids)).or(@notifications.where(action: "follow")).page(params[:page]).per(10)
-
-    # @notifications = current_user_seach(@notifications).page(params[:page]).per(10)
-    # user_seach(@notifications, current_user)
+    @notifications = @notifications.user_seach(@notifications, current_user, comment_ids).page(params[:page]).per(10) #リファクタ
   end
 
   def destroy_all
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,9 @@ module NotificationsHelper
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
+
+  def user_serch2(notifications, current_user)
+    comment_ids = Notification.where(visitor_id: current_user.id),pluck(:user_id)
+    @notifications = notifications.where(user_id: current_user.id).or(notifications.where(user_id: comment_ids)).or(notifications.where(action: "follow"))
+  end
 end
